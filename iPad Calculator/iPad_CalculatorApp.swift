4741ef09cd2deedc948c7b975e5844d3a8e8e6ef
anders0nmat <fullgamefun@gmail.com>
2022-12-22 13:01:58 +0100

    Initial Commit

---
+++ iPad Calculator/iPad_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPad_CalculatorApp.swift
+//  iPad Calculator
+//
+//  Created by Arian Schnappauf on 22.12.22.
+//
+
+import SwiftUI
+
+@main
+struct iPad_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
